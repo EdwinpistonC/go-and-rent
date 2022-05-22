930d9f51efb448225d46ab70f592d2426d0f7844
marcelCarpinter <marcel.carpinter.1995@gmail.com>
2022-05-22 15:20:45 -0300

    Header renders components

---
+++ src/components/organism/Header/Header.test.js
@@ -0,0 +1,31 @@
+import {shallow} from "enzyme";
+
+import {HeaderContainer} from "./StyledComponents";
+import Logo from "../../atom/Logo";
+import Busqueda from "../../atom/Busqueda";
+import HeaderMenu from "../../molecule/HeaderMenu";
+
+let wrapperShallow;
+
+const ROLES = {
+    guest: 'ROLE_GUEST',
+    admin: 'admin',
+    empty: ''
+}
+
+describe("Header Test", () => {
+    it("Renderiza header", () => {
+        wrapperShallow = shallow(
+            <HeaderContainer/>
+        );
+    });
+    it("Renderiza Logo", () => {
+        wrapperShallow.containsMatchingElement(<Logo />);
+    });
+    it("Renderiza Busqueda", () => {
+        wrapperShallow.containsMatchingElement(<Busqueda />);
+    })
+    it("Renderiza Header Menu", () => {
+        wrapperShallow.containsMatchingElement(<HeaderMenu />);
+    })
+});
