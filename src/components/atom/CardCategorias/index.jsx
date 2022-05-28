10b84ed5532876282c305f0d85d05a2d3d7c2887
Edwin Piston <edwinpiston@gmail.com>
2022-05-28 13:18:20 -0300

    fix card

--- src/components/atom/CardCategorias/index.jsx
+++ src/components/atom/CardCategorias/index.jsx
@@ -1,40 +1,43 @@
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import CardActionArea from "@mui/material/CardActionArea";
 import { useNavigate } from "react-router-dom";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const rutaImages = require.context("resources/images/",true);
+const rutaImages = require.context("resources/images/", true);
 
-function CardCategorias({Lugar,Imagen,EnlaceLink}) {
+function CardCategorias({ Lugar, Imagen, EnlaceLink }) {
   //const navegar = useNavigate();
   //navegar.call(Ruta)
-    return (
-      <Link style={{textDecoration:"none", }} to={EnlaceLink}>
-        <Card sx={{ maxWidth: 345 }} >
-          <CardMedia
-            component="img"
-            height="140"
-            image={rutaImages(`./${Imagen}`)}
-            alt="green iguana"
-            
-          />
-          <CardContent >
-            <Typography 
-            
-            gutterBottom variant="h5" component="div" >
+  return (
+    <Link style={{ textDecoration: "none" }} to={EnlaceLink}>
+      <Card
+        sx={{
+          maxWidth: 345,
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          alignContent: "center",
+        }}
+      >
+        <CardMedia
+          component="img"
+          height={"100%"}
+          image={rutaImages(`./${Imagen}`)}
+          alt="green iguana"
+        />
+        <CardContent height={"100%"}>
+          <Typography gutterBottom variant="h5" component="div">
             {Lugar}
-            </Typography>
-          </CardContent>
-
-        </Card>
-      </Link>
-
-    );
-  }
-
+          </Typography>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+}
 
 export { CardCategorias };
