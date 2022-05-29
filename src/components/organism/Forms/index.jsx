656dac68d32258e27cdd317fdb8f16d06a483cf2
Edwin Piston <edwinpiston@gmail.com>
2022-05-29 11:04:16 -0300

    enpoint

--- src/components/organism/Forms/index.jsx
+++ src/components/organism/Forms/index.jsx
@@ -411,3 +411,112 @@ export function FormChangePasswordProfile({ onBack, children, setFields }) {
     </form>
   );
 }
+export function FormEditUser({ onBack, children, setFields }) {
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+    setError,
+    watch,
+  } = useForm({
+    defaultValues: {
+      password: "",
+    },
+  });
+  const [backendError, setBackendError] = React.useState("");
+  const navegar = useNavigate();
+
+  const onSubmit = async (data) => {
+    const api = new Api();
+    api
+      .changePassword({
+        password: data.password,
+      })
+      .then((response, status) => {
+        console.log(response);
+        setBackendError("");
+        navegar("/perfil");
+      })
+      .catch((err) => {
+        if (typeof err.response !== "undefined") {
+          setBackendError("Codigo incorrecto");
+        }
+      });
+    return false;
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      style={{ width: "100%", height: "100%" }}
+    >
+      <Grid
+        container
+        direction="column"
+        justifyContent="space-around"
+        alignItems="stretch"
+        sx={{ px: 2 }}
+        style={{ width: "100%", height: "100%" }}
+      >
+        <Grid item>
+          <Grid item sm sx={{ mt: 2 }}>
+            <TextField
+              type="password"
+              placeholder="contraseña"
+              label="Contraseña"
+              {...register("contrasena1", {
+                validate: {
+                  requerido: (v) => v != "" || "Ingrese la contraseña",
+                },
+              })}
+              error={errors.contrasena1 ? true : false}
+              helperText={errors.contrasena1 && errors.contrasena1.message}
+            />
+          </Grid>
+
+          <Grid item sm sx={{ mt: 2 }}>
+            <TextField
+              type="password"
+              placeholder="Contraseña"
+              label="Repita Contraseña"
+              {...register("contrasena2", {
+                validate: {
+                  requerido: (v) => v != "" || "Repita la contraseña",
+                  iguales: (v) =>
+                    v === watch("contrasena1") ||
+                    "Las contraseñas no coinciden",
+                },
+              })}
+              error={errors.contrasena2 ? true : false}
+              helperText={errors.contrasena2 && errors.contrasena2.message}
+            />
+          </Grid>
+          <Grid item sx={{ mt: 2, my: 2 }}>
+            {children}
+            {backendError != "" ? (
+              <ErrorLabel>{backendError}</ErrorLabel>
+            ) : (
+              <EmptyLabel />
+            )}
+          </Grid>
+        </Grid>
+
+        <Grid
+          item
+          container
+          direction="row"
+          justifyContent="space-around"
+          sx={{ mb: 2 }}
+          alignItems="stretch"
+        >
+          <Grid item>
+            <Button onClick={onBack}>Cancelar</Button>
+          </Grid>
+          <Grid item xs={6}>
+            <Button type="submit">Verificar codigo</Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </form>
+  );
+}
