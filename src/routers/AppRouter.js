422be72a7d226c2b4880f67683fe672a64873332
AgustinPresselLegalHub <aguspressel@gmail.com>
2022-06-04 12:02:26 -0300

    Revert "Revert "Merge branch 'master' into G390-Listado-usuarios""
    This reverts commit 52d766cbfd58bf84e19c76427f559f0ee6db030c.

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,76 +1,79 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import styled from "styled-components";
 
 //React components
-import HomePage from "pages/Home";
-import TestPage from "pages/Test";
 import Header from "components/organism/Header";
 import Footer from "components/organism/Footer";
-import RegisterAdmin from "components/organism/RegisterAdmin";
-import AdminRegister from "../pages/AdminRegister";
+import SideBarMenu from "components/organism/SideBarMenu";
+//HOOK
+import { useLocalStorage, DefaultBusqueda } from "Hooks/LocalStoreHook";
+//PAGES
+import ChangeUserData from "pages/ChangeUserData";
+import Busqueda from "pages/Busqueda";
+import AdminRegister from "pages/AdminRegister";
 import CreateHousing from "pages/CreateHousing";
 import HostHousing from "pages/HostRegister";
+import Profile from "pages/Profile";
+import HomePage from "pages/Home";
+import TestPage from "pages/Test";
+import ChangePassword from "pages/ChangePassword";
+import { RouterContainer, Container } from "./StyledComponents";
 
-const RouterContainer = styled("div")`
-  /* Registro Huésped */
+function HeaderView() {
+  const location = useLocation();
+  console.log(location.pathname);
+}
 
-  /* Auto layout */
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 0px;
-  gap: 58px;
-  /* Background */
-  height: auto;
-
-  position: relative;
-  min-height: 100vh;
-
-  background: #dee7fa;
-`;
-const Container = styled("div")`
-  width: 100%;
-  display: block;
-  min-height: 100%;
-  height: 100%;
-  top: 0;
-  margin: auto;
-`;
 export const AppRouter = ({ children }) => {
+  const [usuario] = useLocalStorage("usuario", "");
+
   return (
     <RouterContainer>
-      <BrowserRouter>
-        <Header />
-        <Container>
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/test" element={<TestPage />} />
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/registrar-anfitrion" element={<HostHousing />} />
+      {usuario.rol === "ROLE_ADMIN" && <SideBarMenu></SideBarMenu>}
+      {usuario.rol !== "ROLE_ADMIN" && <Header />}
 
-            <Route path="/anfitrion">
-              <Route path="new-housing" element={<CreateHousing />} />
-              <Route path="nuevo-alojamiento" element={<CreateHousing />} />
-            </Route>
-            <Route path="/admin">
-              <Route path="new-admin" element={<AdminRegister />} />
-              <Route path="nuevo-admin" element={<AdminRegister />} />
-            </Route>
-            <Route
-              path="*"
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>There's nothing here!</p>
-                </main>
-              }
-            />
-          </Routes>
-          {children}
-        </Container>
-        <Footer />
-      </BrowserRouter>
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/test" element={<TestPage />} />
+          <Route path="/registrar-anfitrion" element={<HostHousing />} />
+
+          <Route path="/perfil" element={<Profile />}></Route>
+
+          <Route
+            path="/perfil/cambiar-contrasena"
+            element={<ChangePassword />}
+          />
+          <Route path="/perfil/editar" element={<ChangeUserData />} />
+
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="edit" element={<ChangeUserData />} />
+          <Route path="change-password" element={<ChangePassword />} />
+          <Route path="/anfitrion">
+            <Route path="new-housing" element={<CreateHousing />} />
+            <Route path="nuevo-alojamiento" element={<CreateHousing />} />
+          </Route>
+          <Route path="/busqueda" element={<Busqueda />}></Route>
+
+          <Route path="/admin">
+            <Route path="new-admin" element={<AdminRegister />} />
+            <Route path="nuevo-admin" element={<AdminRegister />} />
+          </Route>
+
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+        {children}
+      </Container>
+      {usuario.rol !== "ROLE_ADMIN" && <Footer />}
     </RouterContainer>
   );
 };
