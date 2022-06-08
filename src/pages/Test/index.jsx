2e605802442948b6876671fdc7a2868da927110f
AgustinPresselLegalHub <aguspressel@gmail.com>
2022-06-08 16:24:35 -0300

    Lista de usuarios

--- src/pages/Test/index.jsx
+++ src/pages/Test/index.jsx
@@ -3,14 +3,15 @@ import styled from "styled-components";
 import Api from "server/Api";
 import   {  ListaUsuarios } from "components/molecule/ListaUsuarios";
 import Mapa from "components/atom/Mapa";
-import Usuarios from "./users.json"
+//import Usuarios from "./users.json"
 
+import { useState, useEffect } from "react";
 
 const Container = styled("div")``;
 const backend = new Api();
 //let listaDeUsuarios = backend.listadoUsuarios()
 
-console.log(Usuarios["usuarios"]);
+//console.log(Usuarios["usuarios"]);
 backend.listadoUsuarios().then((response)=>{
  console.log(response.data)
 
@@ -19,9 +20,22 @@ backend.listadoUsuarios().then((response)=>{
 
 //console.log(backend.listadoUsuarios())
 export default function TestPage() {
+  const [usuarios, setUsuarios] = useState([]);
+
+
+  const getData = async () => {
+    await backend.listadoUsuarios().then((response)=>{
+      console.log(response.data["usuarios"])
+      setUsuarios(response.data["usuarios"])
+     })
+  };
+  
+   useEffect(() => {
+    getData()
+  }, []);
   return <Container>
       <ListaUsuarios 
-      datos={Usuarios["usuarios"]} />
+      datos={usuarios} />
 
   </Container>;
 }
