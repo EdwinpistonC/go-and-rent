851a01d6d7540bf0bb35f31322ea6a03d787e5d6
Edwin Piston <edwinpiston@gmail.com>
2022-06-20 16:42:28 -0300

    editar, cambios para los inputs

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -18,10 +18,10 @@ import ChangePassword from "pages/ChangePassword";
 import { RouterContainer, Container } from "./StyledComponents";
 import DetalleAlojamiento from "pages/DetalleAlojamiento";
 import UsuariosPage from "pages/Usuarios";
-import AprobarUsuario from "pages/AprobarUsuario"
+import AprobarUsuario from "pages/AprobarUsuario";
 import { ListaReservas } from "pages/Reservas";
 import AdministrarReservas from "pages/AdministrarReservas";
-import EditarReserva from "pages/EditarReserva";
+import EditarAlojamiento from "pages/EditarAlojamiento";
 import Mensajeria from "pages/Mensajeria";
 import Estadisticas from "pages/Estadisticas";
 
@@ -35,12 +35,11 @@ export const AppRouter = ({ children }) => {
         <Routes>
           <Route path="/reservas" element={<ListaReservas />} />
           <Route path="/reservas/:id" element={<AdministrarReservas />} />
-          <Route path="/reservas/editar/:id" element={<EditarReserva />} />
+          <Route path="/reservas/editar/:id" element={<EditarAlojamiento />} />
 
           <Route path="/" element={<HomePage key={Date.now()} />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/registrar-anfitrion" element={<HostHousing />} />
-          
 
           <Route path="/mensajeria" element={<Mensajeria />} />
 
@@ -67,7 +66,10 @@ export const AppRouter = ({ children }) => {
           <Route path="/admin">
             <Route path="nuevo-admin" element={<AdminRegister />} />
             <Route path="usuarios" element={<UsuariosPage />} />
-            <Route path="aprobarUsuarios/:id/:alias" element={<AprobarUsuario />} />
+            <Route
+              path="aprobarUsuarios/:id/:alias"
+              element={<AprobarUsuario />}
+            />
           </Route>
           <Route
             path="*"
